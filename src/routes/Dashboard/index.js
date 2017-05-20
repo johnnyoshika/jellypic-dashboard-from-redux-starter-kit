d20536f176f435b082144c8e016ef9dcde0ad794
Johnny Oshika <johnnyoshika@gmail.com>
2017-05-20 12:58:15 -0700

    Add authenticated gate in Dashboard route.

--- src/routes/Dashboard/index.js
+++ src/routes/Dashboard/index.js
@@ -8,5 +8,14 @@ export default {
   indexRoute  : Home,
   childRoutes : [
     Profile
-  ]
+  ],
+  onEnter(nextState, replace) {
+    // TODO: redirect if not logged in
+    // example: https://github.com/ReactTraining/react-router/tree/4f2746155ce1cb6bd194864f9feb1599b2e46c51/examples/auth-flow
+    if (false)
+      replace({
+        pathname: '/login',
+        state: { nextPathName: nextState.location.pathname }
+      });
+  }
 }
