71bdf9ef9a64ddc9b9334c66135d4eb78334af31
Johnny Oshika <johnnyoshika@gmail.com>
2017-05-22 14:09:51 -0700

    Bind methods in LoginView constructor to pass our linter.

--- src/routes/Login/components/LoginView.js
+++ src/routes/Login/components/LoginView.js
@@ -4,6 +4,14 @@ import ErrorMessage from '../../../components/ErrorMessage'
 import './LoginView.scss'
 
 export class LoginView extends Component {
+  constructor () {
+    super()
+
+    // REACT ES6 classes don't autobind, so bind it in the constructor
+    // as suggested here: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md#es6-classes
+    this.onClick = this.onClick.bind(this)
+  }
+
   componentWillMount () {
     this.props.checkFacebook()
   }
@@ -17,7 +25,7 @@ export class LoginView extends Component {
     return this.props.login.state === 'processing' || this.props.login.state === 'success'
   }
 
-  onClick () {
+  onClick (arg1, arg2, arg3) {
     this.props.loginWithFacebook()
   }
 
@@ -30,7 +38,7 @@ export class LoginView extends Component {
             Jellypic
           </div>
           <div className="text-center">
-            <button className="btn btn-primary btn-lg" disabled={this.disabled()} onClick={this.onClick.bind(this)}>Log in with Facebook</button>
+            <button className="btn btn-primary btn-lg" disabled={this.disabled()} onClick={this.onClick}>Log in with Facebook</button>
             {this.props.login.state === 'error' && <ErrorMessage message={this.props.login.error} />}
           </div>
         </div>
