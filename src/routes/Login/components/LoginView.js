150d742b47bfc238f8bcf5ecd1eab161cb157fdf
Johnny Oshika <johnnyoshika@gmail.com>
2017-05-22 03:39:13 -0700

    Switch LoginView to a stateful component to prepare for authentication.

--- src/routes/Login/components/LoginView.js
+++ src/routes/Login/components/LoginView.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './LoginView.scss'
 
-export const LoginView = () => (
-  <div className="login-container">
-    <div className="gutter"></div>
-    <div className="login-main">
-      <div className="font-lobster text-center mb-40">
-        Jellypic
+export class LoginView extends Component {
+  render() {
+    return (
+      <div className="login-container">
+        <div className="gutter"></div>
+        <div className="login-main">
+          <div className="font-lobster text-center mb-40">
+            Jellypic
+          </div>
+          <div className="text-center">
+            <a href="" className="btn">Log in with Facebook</a>
+          </div>
+        </div>
+        <div className="gutter"></div>
       </div>
-      <div className="text-center">
-        <a href="" className="btn">Log in with Facebook</a>
-      </div>
-    </div>
-    <div className="gutter"></div>
-  </div>
-)
+    )
+  }
+}
 
 export default LoginView
