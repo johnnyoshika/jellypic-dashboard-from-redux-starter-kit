87e619e80db11f666867368c6fb7e67dc2dab1fa
Johnny Oshika <johnnyoshika@gmail.com>
2017-09-16 23:52:25 -0700

    Put sw.js under lint control.

--- src/sw.js
+++ src/sw.js
@@ -1,41 +1,41 @@
-const version = 'v2';
+const version = 'v5'
 
-self.addEventListener('install', function(event) {
-  console.log('SW version %s Installed at', version, new Date().toLocaleTimeString());
+self.addEventListener('install', function (event) {
+  console.log('SW version %s Installed at', version, new Date().toLocaleTimeString())
   event.waitUntil(
     caches.open(version)
-    .then(function(cache) {
+    .then(function (cache) {
       return cache.addAll([
         '/offline/',
         '/offline/dinosaur.gif'
-      ]);
-    }));
-});
+      ])
+    }))
+})
 
-self.addEventListener('activate', function(event) {
-  console.log('SW version %s Activated at', version, new Date().toLocaleTimeString());
+self.addEventListener('activate', function (event) {
+  console.log('SW version %s Activated at', version, new Date().toLocaleTimeString())
   event.waitUntil(
     caches.keys()
-    .then(function(keys) {
-      return Promise.all(keys.filter(function(key){
-        return key !== version;
-      }).map(function(key){
-        return caches.delete(key);
-      }));
-    }));
-});
+    .then(function (keys) {
+      return Promise.all(keys.filter(function (key) {
+        return key !== version
+      }).map(function (key) {
+        return caches.delete(key)
+      }))
+    }))
+})
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         if (response)
-          return response;
+          return response
 
         // reliable to check not onLine, but b/c of LiFi, can't rely on is onLine.
         if (!navigator.onLine)
-          return caches.match(new Request('/offline/'));
+          return caches.match(new Request('/offline/'))
 
-        return fetch(event.request);
-      }));
-});
+        return fetch(event.request)
+      }))
+})
