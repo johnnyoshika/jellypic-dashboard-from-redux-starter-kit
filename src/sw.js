03f7b3d29c05291032e3438d7670e9e0ce008859
Johnny Oshika <johnnyoshika@gmail.com>
2017-09-16 23:58:22 -0700

    Cache all resources on demand.

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const version = 'v5'
+const version = 'v6'
 
 self.addEventListener('install', function (event) {
   console.log('SW version %s Installed at', version, new Date().toLocaleTimeString())
@@ -26,16 +26,41 @@ self.addEventListener('activate', function (event) {
 })
 
 self.addEventListener('fetch', function (event) {
+  console.log('fetch', event.request.url)
   event.respondWith(
     caches.match(event.request)
       .then(function (response) {
-        if (response)
+        if (response) {
+          console.log('retrieving from cache', event.request.url)
           return response
+        }
 
         // reliable to check not onLine, but b/c of LiFi, can't rely on is onLine.
         if (!navigator.onLine)
           return caches.match(new Request('/offline/'))
 
-        return fetch(event.request)
+        return fetchAndUpdate(event.request)
       }))
 })
+
+var fetchAndUpdate = function (request) {
+  return fetch(request)
+    .then(function (response) {
+      if (!response)
+        return
+
+      if (!response.ok) {
+        console.log('not ok, not caching', request.url, response.status)
+        return response
+      }
+
+      console.log('ok, caching', request.url, response.status)
+      return caches.open(version)
+        .then(function (cache) {
+          return cache.put(request, response.clone())
+            .then(function () {
+              return response
+            })
+        })
+    })
+}
