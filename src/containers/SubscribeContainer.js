254de8d60d8f72b5dd4f5deb5d6cbae9d2899b47
Johnny Oshika <johnnyoshika@gmail.com>
2018-01-30 23:54:28 -0800

    Add subscribe to notification.

---
+++ src/containers/SubscribeContainer.js
@@ -0,0 +1,10 @@
+import { connect } from 'react-redux'
+import Subscribe from '../components/Subscribe'
+
+const mapDispatchToProps = {
+}
+
+const mapStateToProps = (state) => ({
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Subscribe)
