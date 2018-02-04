dca98606db1543cfb4e3752a12218267de58914a
Johnny Oshika <johnnyoshika@gmail.com>
2018-02-04 12:23:01 -0800

    Service worker: listen to push and nogicationclick events.

--- src/sw.js
+++ src/sw.js
@@ -73,3 +73,34 @@ var fetchAndUpdate = function (request) {
         })
     })
 }
+
+self.addEventListener('push', e => {
+  var payload = e.data.json()
+  e.waitUntil(
+    self.registration.showNotification(payload.title, {
+      body: payload.message,
+      icon: payload.icon,
+      // badge: '',
+      data: payload,
+      actions: [
+        { action: 'view', title: 'View Post'/*, icon: ''*/ },
+        { action: 'app', title: 'Open App'/*, icon: ''*/ },
+      ]
+    }))
+})
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close()
+  var payload = e.notification.data
+
+  switch (e.action) {
+    case 'app':
+      e.waitUntil(
+        clients.openWindow(e.target.location.origin))
+      break
+    case 'view':
+    default:
+      e.waitUntil(
+        clients.openWindow(`${e.target.location.origin}/posts/${payload.postId}`))
+  }
+})
